f548533682de1a353846642ca4dd98d4ab5eff44
TomBANCHEREAU <tom.banchereau@viacesi.fr>
2022-06-17 10:22:58 +0200

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import 'express-async-errors'
+import { auth } from './auth'
+// import SponsorCodeRouter from './routes/SponsorCodeRouter'
+// import SponsorshipRouter from './routes/SponsorshipRouter'
+
+const app = express()
+
+app.use(auth)
+// app.use('/api/sponsorships', SponsorshipRouter)
+// app.use('/api/sponsor-code', SponsorCodeRouter)
+
+export default app
